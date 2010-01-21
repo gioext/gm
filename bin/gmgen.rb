fd9c17627b5b1cabb5f96e81d3e502fbda1bdcf0
Kazuki UCHIDA <gioext@gmail.com>
2010-01-22 00:28:43 +0900

    add template

--- bin/gmgen.rb
+++ bin/gmgen.rb
@@ -8,24 +8,55 @@ if ARGV[0] == 'controller' && ARGV[1]
     exit
   end
 
+  name = ARGV[1].capitalize
+
+  if File.exist?("app/controllers/#{name.downcase}.py") || File.exist?("app/templates/#{name.downcase()}")
+    puts "already exist #{name}"
+    exit
+  end
+
   dir = File.join(File.dirname(__FILE__), 'erb')
   controller = File.join(dir, 'controller.erb')
   template = File.join(dir, 'template.erb')
-  name = ARGV[1].capitalize
-
   controller_body = ERB.new(File.read(controller)).result
   template_body = ERB.new(File.read(template)).result
 
+  Dir.mkdir("app/templates/#{name.downcase()}")
   open("app/controllers/#{name.downcase()}.py", "w") do |f|
     f.write controller_body
   end
-  Dir.mkdir("app/templates/#{name.downcase()}") unless File.exist?("app/templates/#{name.downcase()}")
   open("app/templates/#{name.downcase()}/index.html", "w") do |f|
     f.write template_body
   end
 
   puts "+ app/controllers/#{name.downcase()}.py"
   puts "+ app/templates/#{name.downcase()}/index.html"
+elsif ARGV[0] == 'template' && ARGV[1]
+  unless File.exist?('app/templates')
+    puts "please execute root dir."
+    exit
+  end
+  controller = ARGV[1].downcase
+  action = ARGV[2].downcase
+  if !File.exist?("app/templates/#{controller}")
+    puts "no controller #{controller}"
+    exit
+  end
+  if File.exist?("app/templates/#{controller}/#{action}.html")
+    puts "already exist #{controller}/#{action}.html"
+    exit
+  end
+
+  name = action
+  dir = File.join(File.dirname(__FILE__), 'erb')
+  template = File.join(dir, 'template.erb')
+  template_body = ERB.new(File.read(template)).result
+
+  open("app/templates/#{controller}/#{action}.html", "w") do |f|
+    f.write template_body
+  end
+
+  puts "+ app/templates/#{controller}/#{action}.html"
 else
   puts "usage: gmgen.rb controller helloworld"
 end
